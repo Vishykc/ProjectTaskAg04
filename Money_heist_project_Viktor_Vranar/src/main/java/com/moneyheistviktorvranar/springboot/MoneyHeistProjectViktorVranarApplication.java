ed3dddff6699a831f171547d136b605bcfc9262e
Viktor Vranar <viktor.vranar@gmail.com>
2021-06-30 00:09:05 +0200

    Made a new Spring Boot project and made some configurations

---
+++ Money_heist_project_Viktor_Vranar/src/main/java/com/moneyheistviktorvranar/springboot/MoneyHeistProjectViktorVranarApplication.java
@@ -0,0 +1,13 @@
+package com.moneyheistviktorvranar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHeistProjectViktorVranarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyHeistProjectViktorVranarApplication.class, args);
+	}
+
+}
